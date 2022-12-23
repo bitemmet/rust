b36cd7b37098b537552653d554160c60a64380bd
bitemmet <bitemmet@proton.me>
2022-12-23 01:03:41 +0000

    add helix editor

--- setup/main.sh
+++ setup/main.sh
@@ -1,1 +1,26 @@
-rustup default nightly
+
+this_script_dir_path=$(realpath  $(dirname ${BASH_SOURCE[0]}))
+
+
+function setup_environment() {
+    export PATH="/workspace/rust/setup/bin/:$PATH"
+}
+
+
+function install_rust() {
+    rustup default nightly
+}
+
+function install_helix() {
+    cd ${this_script_dir_path}/build
+    git clone https://github.com/helix-editor/helix.git
+    cd helix
+    cargo install --path helix-term
+}
+
+
+function main() {
+    setup_environment
+}
+
+main
